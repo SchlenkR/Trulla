77b3f5c10bdccfdbd85481fc39270e940dece9cb
SchlenkR <ronald.schlenker@binarygears.de>
2022-05-16 21:50:54 +0200

    asdasdasd

--- TpTest/script.fsx
+++ TpTest/script.fsx
@@ -2,5 +2,5 @@
 #I "../src/Trulla.TypeProvider/bin/Debug/netstandard2.0"
 #r "Trulla.TypeProvider.dll"
 
-type T = TrullaProvider.GenerativeProvider<Count = 10>
-T().Property1
+type T = TrullaProvider.GenerativeProvider<Count = 2>
+T("xxx").Property2
